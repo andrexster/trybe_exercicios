6e0080516733d67f2632a581f4f7402964682317
andrexster <zoio1@hotmail.com>
2023-01-19 20:43:29 -0300

    inserindo palavras

--- cacaPalavras/script.js
+++ cacaPalavras/script.js
@@ -1,3 +1,7 @@
+const arrayWords = ['Laranja', 'Pera', 'Goiaba', 'Uva', 'Manga'];
+
+
+//função para gerar as celulas da matriz
 const generateCells = () => {
   const matriz = document.querySelector('.matriz');
   console.log(matriz);
@@ -14,3 +18,24 @@ const generateCells = () => {
 }
 
 generateCells();
+
+//Função que gera palavras aleatórias
+const addWords = () =>{
+  const lines = document.querySelectorAll('.line');
+  const indexLine = Math.floor(Math.random() * lines.length);
+
+  const indexWords = Math.floor(Math.random() * arrayWords.length);
+  const word = arrayWords[indexWords];
+
+  const cells = lines[indexLine].querySelectorAll('.cell');
+  const indexCell = Math.floor(Math.random() * (cells.length - word.length + 1));
+
+  for(let index = 0; index < word.length; index +=1 ){
+    cells[indexCell + index].innerText = word[index];
+  }
+
+}
+
+for(let index = 0; index < arrayWords.length; index +=1){
+  addWords();
+}
