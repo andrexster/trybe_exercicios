45ce0454fdb4e5ed9072e7de99b8efbb9d5571cf
andrexster <zoio1@hotmail.com>
2023-01-19 23:12:28 -0300

    terminando

--- cacaPalavras/script.js
+++ cacaPalavras/script.js
@@ -1,5 +1,11 @@
-const arrayWords = ['Laranja', 'Pera', 'Goiaba', 'Uva', 'Manga'];
+const arrayWords = ['LARANJA', 'PERA', 'GOIABA', 'UVA', 'MANGA'];
+let wordSelected = '';
 
+const paintCell = (event) => {
+
+  wordSelected += event.target.innerText;
+  event.target.classList.add('selected');
+}
 
 //função para gerar as celulas da matriz
 const generateCells = () => {
@@ -11,6 +17,7 @@ const generateCells = () => {
     for(let index1 = 0; index1 < 10; index1 +=1){
       const cell = document.createElement('div');
       cell.className = 'cell';
+      cell.addEventListener('click', paintCell);
       line.appendChild(cell);
     }
     matriz.appendChild(line);
@@ -19,11 +26,13 @@ const generateCells = () => {
 
 generateCells();
 
+
 //Função que gera palavras aleatórias
 const addWords = () =>{
   const lines = document.querySelectorAll('.line');
   const indexLine = Math.floor(Math.random() * lines.length);
 
+  //Sorteia uma das palavras
   const indexWords = Math.floor(Math.random() * arrayWords.length);
   const word = arrayWords[indexWords];
 
@@ -34,8 +43,26 @@ const addWords = () =>{
     cells[indexCell + index].innerText = word[index];
   }
 
+  lines[indexLine].className = 'lineok';
+  arrayWords.splice(indexWords, 1);
+  console.log(arrayWords);
 }
 
-for(let index = 0; index < arrayWords.length; index +=1){
+//Insere letras aleatorias
+const randomLetters = () => {
+  const cell = document.querySelectorAll('.cell');
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZ';
+
+  for(let index = 0; index < cell.length; index += 1){
+    if(cell[index].innerText === ''){
+      const letter = letters.charAt(Math.floor(Math.random() * letters.length));
+      cell[index].innerText = letter;
+    }
+  }
+}
+
+randomLetters();
+
+for(let index = arrayWords.length; index > 0; index +=1){
   addWords();
 }
